54caa1bb1326227a7f5ed2e6a8f098c7db9a93d8
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-04 16:30:34 +0200

    first commit

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Category;
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++){
+            $new_post = new Post();
+
+            $tag_ids = Tag::pluck('id')->toArray();
+            $categories_ids = Category::pluck('id')->toArray();
+            $users_ids = User::pluck('id')->toArray();
+
+            $new_post->title = $faker->sentence();
+            $new_post->user_id = Arr::random($users_ids);
+            $new_post->content = $faker->paragraph(2);
+            $new_post->image = $faker->word('animals',true);
+            $new_post->category_id = Arr::random($categories_ids);
+
+            $new_post->save();
+
+            $post_tags = [];
+
+            foreach($tag_ids as $tag){
+                if($faker->boolean()) $post_tags[] = $tag;
+            }
+
+            $new_post->tags()->attach($post_tags);
+        }
+    }
+}
---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="suspance">Coming soon...</div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {},
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+#suspance {
+    font-size: 50px;
+    text-align: center;
+}
+</style>
